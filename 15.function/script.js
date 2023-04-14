c42b7c1c552520bb944e528fb1a11cc6ce60a100
Gunay HUmmatova <hummatovagunay02@gmail.com>
2023-04-15 00:47:44 +0400

    task added

--- 15.function/script.js
+++ 15.function/script.js
@@ -44,3 +44,60 @@ addition(-3,-6)
 addition(7,3)
 
 /////////////// 5.Less Than 100?
+// Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false. (used: Arrow function)
+// (x, y)=>{
+//     if(x+y<100){
+//        return true;
+//     }else{
+//         return false;
+//     }
+// };
+// console.log(20,85)
+
+//////////////// 6.Volume of a Box
+// Create a function that takes an object argument sizes (contains width, length, height keys)
+//  and returns the multiplication sizes of the box.
+// Don't forget to return the result.
+function volumeOfObj(width,length,height) {
+    let volume= width*length*height;
+    return volume
+}console.log(volumeOfObj(3,4,5))
+
+
+///////////////// 7.Basketball Points
+// You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, find the final points for the team and return that value.
+// Create a function called "points(twoPointers, threePointers)" and add two parameters to it.
+function basketPoint(twoPointers,threePointers) {
+    let score = twoPointers*2 + threePointers*3;
+    return score
+}
+console.log(basketPoint(1, 1));
+console.log(basketPoint(7, 5));
+console.log(basketPoint(38, 8));
+// 8.Create a function called 'fillInfo()', which is used to change "my name" to 'Ulfat' ,'my surname' to 'Zakirli', 'my city' to 'Baku'.
+// (used: for...of).
+const arr4=[
+    {
+        name:'my name',
+        surname:'my surname',
+        city:'my city'
+    },
+    {
+        name:'my name',
+        surname:'my surname',
+        city:'my city'
+    },
+    {
+        name:'my name',
+        surname:'my surname',
+        city:'my city'
+    },
+]
+function fillInfo(smth) {
+    for (let item of smth){
+        item.smth ="Ulfat";
+        item.surname ="Zakirli";
+        item.city="Baku"
+    }
+    return smth;
+}console.log((fillInfo(arr4)))
