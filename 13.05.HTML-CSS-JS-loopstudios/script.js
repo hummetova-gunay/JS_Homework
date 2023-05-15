a591d276c4b7fe3841df1bdf1ca2ffea1d1a7fe6
Gunay HUmmatova <hummatovagunay02@gmail.com>
2023-05-15 23:54:09 +0400

    task added

---
+++ 13.05.HTML-CSS-JS-loopstudios/script.js
@@ -0,0 +1,7 @@
+// let menuIcon=document.querySelector('.menu-icon')
+// let newNav=document.querySelector('.new-nav')
+// menuIcon.addEventListener('click', function(){
+//     newNav.classList.toggle='new-nav'
+//     newNav.style.display='block'
+//     console.log(newNav);
+// })
